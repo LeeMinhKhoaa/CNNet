3a7f8094f1708abf3ed8091f8e8dc3bb5095335e
LeeMinhKhoaa <leminhkhoa5241@gmail.com>
2023-11-12 15:23:05 +0700

    initial commit

---
+++ Nhom13/Nhom13/frm_Home.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Nhom13
+{
+    partial class frm_Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1125, 529);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(206)))), ((int)(((byte)(230)))));
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1117, 500);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Home";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1117, 500);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tạo hóa đơn";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1117, 500);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tạo phiếu nhập";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1117, 500);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Thống kê";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 529);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
---
+++ database/Đồ Án.sql
@@ -0,0 +1,156 @@
+﻿CREATE DATABASE NHATHUOC
+USE NHATHUOC
+-- Tạo bảng HoaDon
+CREATE TABLE HOADON (
+    MaHD varchar(10) PRIMARY KEY,
+    NgayLap DATE,
+    TienThua DECIMAL(10,2),
+    TienThu DECIMAL(10,2),
+    TongTien DECIMAL(10,2),
+	MaKH varchar(10),
+	MaNV varchar(10)
+
+);
+
+
+
+-- Tạo bảng KhachHang
+CREATE TABLE KHACHHANG (
+    MaKH varchar(10) PRIMARY KEY,
+	TenKH NVARCHAR(200),
+    GioiTinh VARCHAR(10),
+    Tuoi INT,
+    SDT VARCHAR(20),
+);
+
+-- Tạo bảng NhanVien
+CREATE TABLE NHANVIEN (
+    MaNV varchar(10) PRIMARY KEY,
+    TenNV NVARCHAR(50),
+    GioiTinh VARCHAR(10),
+    SDT VARCHAR(20),
+    ChucVu NVARCHAR(50),
+    TaiKhoan VARCHAR(50),
+    MatKhau VARCHAR(50),
+
+);
+
+
+-- Tạo bảng PhieuNhap
+CREATE TABLE PHIEUNHAP (
+    MaPN VARCHAR(10) PRIMARY KEY,
+    NgayNhap DATE,
+    NguoiGiao VARCHAR(50),
+    TongTien DECIMAL(10,2),
+	MaNV VARCHAR(10),
+	MaNCC VARCHAR(10)
+
+
+);
+-- Tạo bảng ChiTietPhieuNhap
+CREATE TABLE CHITIETPHIEUNHAP (
+    MaPN VARCHAR(10) not null,
+    MaThuoc VARCHAR(10) not null,
+    SoLuong INT,
+    DonGiaNhap DECIMAL(10,2),
+	MaLo varchar(10)
+	PRIMARY KEY(MaPN,MaThuoc)
+);
+
+-- Tạo bảng Thuoc
+CREATE TABLE THUOC (
+    MaThuoc VARCHAR(10) PRIMARY KEY,
+	MaNhomThuoc VARCHAR(10),
+	MaLoaiThuoc varchar(10),
+    DVT VARCHAR(20),
+    GiaBan DECIMAL(10,2),
+    HanSuDung DATE,
+    MoTa NVARCHAR(100),
+    GiaNhap DECIMAL(10,2),
+    HinhAnh VARCHAR(100),
+    TacDungPhu NVARCHAR(100),
+    XuatXu NVARCHAR(50),
+
+);
+
+
+-- Tạo bảng NhaCungCap
+CREATE TABLE NHACUNGCAP (
+    MaNCC VARCHAR(10) PRIMARY KEY,
+    TenNCC NVARCHAR(100),
+    DiaChi NVARCHAR(100),
+    SDT VARCHAR(20),
+
+);
+
+
+CREATE TABLE NHOMTHUOC(
+	MaNhomThuoc VARCHAR(10) PRIMARY KEY,
+	TenNhomThuoc NVARCHAR(200)
+)
+
+CREATE TABLE CHITIETHD(
+MaHD VARCHAR(10) ,
+MaThuoc VARCHAR(10),
+SoLuong INT,
+DonGia DECIMAL(10,2)
+primary key(MaHD,MaThuoc)
+)
+create table LOTHUOC(
+	MaLo varchar(10) primary key,
+	SoLo int,
+	SoLuongTon int,
+	NgayHetHan datetime,
+)
+
+-- Liên kết khóa ngoại từ bảng HoaDon đến bảng KhachHang
+ALTER TABLE HoaDon
+ADD FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH);
+
+-- Liên kết khóa ngoại từ bảng HoaDon đến bảng NhanVien
+ALTER TABLE HoaDon
+ADD FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV);
+
+ 
+
+
+-- Liên kết khóa ngoại từ bảng PhieuNhap đến bảng NhanVien
+ALTER TABLE PhieuNhap
+ADD FOREIGN KEY (MaNV) REFERENCES NhanVien(MaNV);
+
+
+
+-- Liên kết khóa ngoại từ bảng ChiTietPhieuNhap đến bảng PhieuNhap
+ALTER TABLE ChiTietPhieuNhap
+ADD FOREIGN KEY (MaPN) REFERENCES PhieuNhap(MaPN);
+
+-- Liên kết khóa ngoại từ bảng ChiTietPhieuNhap đến bảng Thuoc
+ALTER TABLE ChiTietPhieuNhap
+ADD FOREIGN KEY (MaThuoc) REFERENCES Thuoc(MaThuoc);
+
+-- Liên kết khóa ngoại từ bảng CHITIEUHD đến bảng HoaDon
+ALTER TABLE CHITIETHD
+ADD FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD);
+
+-- Liên kết khóa ngoại từ bảng CHITIEUHD đến bảng Thuoc
+ALTER TABLE CHITIETHD
+ADD FOREIGN KEY (MaThuoc) REFERENCES Thuoc(MaThuoc);
+
+-- Liên kết khóa ngoại từ bảng Thuoc đến bảng NhomThuoc
+ALTER TABLE Thuoc
+ADD FOREIGN KEY (MaNhomThuoc) REFERENCES NHOMTHUOC(MaNhomThuoc);
+
+
+-- Liên kết khóa ngoại từ bảng PhieuNhap đến bảng NhaCungCap
+ALTER TABLE PhieuNhap
+ADD FOREIGN KEY (MaNCC) REFERENCES NhaCungCap(MaNCC);
+
+-- Liên kết khóa ngoại Lo đến ChiTietPhieuNhap
+alter table ChiTietPhieuNhap
+add foreign key(MaLo) references LoThuoc(MaLo)
+
+alter table Thuoc 
+add MaNCC varchar(10)
+
+alter table Thuoc 
+add foreign key (MaNCC) references NhaCungCap(MaNCC)
