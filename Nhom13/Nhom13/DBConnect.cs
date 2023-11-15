f978448c05f684a2ea35da1a930de09e15bdcefe
boquangkhai <quangkhai221203@gmail.com>
2023-11-15 18:45:44 +0700

    add class DBConnect

---
+++ Nhom13/Nhom13/DBConnect.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nhom13
+{
+    internal class DBConnect
+    {
+        SqlConnection connect;
+
+        public SqlConnection Connect
+        {
+            get { return connect; }
+            set { connect = value; }
+        }
+        string strConnect = @"Data Source=QKHAI\SQLEXPRESS;Initial Catalog=QLHANGHOA;Integrated Security=True";
+        public DBConnect()
+        {
+            connect = new SqlConnection(strConnect);
+        }
+
+        public void open()
+        {
+            if (connect.State == ConnectionState.Closed)
+                connect.Open();
+        }
+        public void close()
+        {
+            if (connect.State == ConnectionState.Open)
+                connect.Close();
+        }
+        public int getNonQuery(string sql)
+        {
+            open();
+            SqlCommand cmd = new SqlCommand(sql, connect);
+            int kq = cmd.ExecuteNonQuery();
+            close();
+            return kq;
+        }
+
+        public object getScalar(string sql)
+        {
+            open();
+            SqlCommand cmd = new SqlCommand(sql, connect);
+            object kq = cmd.ExecuteScalar();
+            close();
+            return kq;
+        }
+
+        public DataTable getDatatable(string sql)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, connect);
+            da.Fill(dt);
+            return dt;
+        }
+
+        public int updateDatabase(string sql, DataTable dt)
+        {
+            SqlDataAdapter da_lop = new SqlDataAdapter(sql, Connect);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da_lop);
+            int kq = da_lop.Update(dt);
+            return kq;
+        }
+    }
+}
